327a6c3debe1f22feb33d77c1ae8db05f08ceba2
benco <20131256@ce.pucmm.edu.do>
2019-09-14 19:10:39 -0400

    Chequeando usuarios conectados

--- src/main/resources/static/js/angularjs/controllers/PosController.js
+++ src/main/resources/static/js/angularjs/controllers/PosController.js
@@ -2,61 +2,20 @@
  * Created by Dany on 09/05/2019.
  */
 app.controller("posController", ['$http', 'PosService', '$scope', 'DTOptionsBuilder', function ($http, PosService, $scope, DTOptionsBuilder) {
-    $scope.pos = [];
+
+    $scope.global = {
+        dtOptions: loadDT(),
+        pos: [],
+
+    }
     $scope.serial = '';
     $scope.pageno = 1;
     $scope.itemsPerPage = 1000;
     $scope.state = 1;
     // http://blog.ashwani.co.in/new/2015/12/07/How-to-use-Angular-Datatables.html
-    $scope.dtOptions = DTOptionsBuilder.newOptions()
-        .withDisplayLength(5)
-        .withBootstrapOptions(
-            {
-                pagination:{
-                    classes:{
-                        ul: 'pagination pagination-sm'
-                    }
-                }
-            }
-        )
-        .withOption("destroy", true)
-        .withOption('responsive', true)
-        .withOption('scrollX', '100%')
-        .withOption('deferRender', true)
-        .withColumnFilter({
-            aoColumns: [
-                {
-                type: 'number'
-                },
-                    {
-                    type: 'text',
-                    bRegex: true,
-                    bSmart: true
-                },
-                {
-                    type: 'text',
-                    bRegex: true,
-                    bSmart: true
-                },
-                {
-                    type: 'text',
-                    bRegex: true,
-                    bSmart: true
-                },
-                {
-                    type: 'select',
-                    bRegex: false,
-                    values: ['Wi','Non']
-                },
-                {
-                    type: 'select',
-                    bRegex: false,
-                    values: ['Wi','Non']
-                }
-            ]
-        });
 
     fetchAllPos();
+
 
     $scope.getData = function () {
         fetchAllPosFiltered($scope.pageno, $scope.itemsPerPage, $scope.state);
@@ -67,13 +26,66 @@ app.controller("posController", ['$http', 'PosService', '$scope', 'DTOptionsBuil
             .then(
                 function (d) {
                     if (d === null || d === undefined)
-                        $scope.pos = [];
-                    else
-                        $scope.pos = d;
+                        $scope.global.pos = [];
+                    else{
+                        $scope.global.pos = d;
+                        loadDT();
+                    }
+
                 },
                 function (errorResponse) {
                     console.error(errorResponse);
                 })
+    }
+
+    function loadDT() {
+        return DTOptionsBuilder.newOptions()
+            .withDisplayLength(5)
+            .withBootstrapOptions(
+                {
+                    pagination:{
+                        classes:{
+                            ul: 'pagination pagination-sm'
+                        }
+                    }
+                }
+            )
+            .withOption("destroy", true)
+            .withOption('responsive', true)
+            .withOption('scrollX', '100%')
+            .withOption('deferRender', true)
+            .withColumnFilter({
+                aoColumns: [
+                    {
+                        type: 'number'
+                    },
+                    {
+                        type: 'text',
+                        bRegex: true,
+                        bSmart: true
+                    },
+                    {
+                        type: 'text',
+                        bRegex: true,
+                        bSmart: true
+                    },
+                    {
+                        type: 'text',
+                        bRegex: true,
+                        bSmart: true
+                    },
+                    {
+                        type: 'select',
+                        bRegex: false,
+                        values: ['Wi','Non']
+                    },
+                    {
+                        type: 'select',
+                        bRegex: false,
+                        values: ['Wi','Non']
+                    }
+                ]
+            });
     }
 
     function fetchAllPosFiltered(pageno, itemsPerPage, state) {
@@ -81,9 +93,9 @@ app.controller("posController", ['$http', 'PosService', '$scope', 'DTOptionsBuil
             .then(
                 function (d) {
                     if (d === null || d === undefined)
-                        $scope.pos = [];
+                        $scope.global.pos = [];
                     else
-                        $scope.pos = d.content;
+                        $scope.global.pos = d.content;
                 },
                 function (errorResponse) {
                     console.error(errorResponse);

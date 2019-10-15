3f031772e53ec3d77837a834db610e9a578bd2b1
Pierre Dany Ridore <pridore@advansys.com.do>
2019-10-15 13:11:29 -0400

    fix

---
+++ src/main/resources/static/js/angularjs/controllers/GlobalConfigurationController.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Dany on 09/05/2019.
+ */
+app.controller("globalConfigurationController", ['ConfigurationService','$scope',function (ConfigurationService, $scope) {
+    $scope.global = {
+
+    };
+
+}]);

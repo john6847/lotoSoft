eb1c46ed09f7f5770e2d7d322053024da10549d7
benco <20131256@ce.pucmm.edu.do>
2019-10-27 15:41:03 -0400

    created jar file

--- src/main/java/com/b/r/loteriab/r/Controllers/WebMvcConfig.java
+++ src/main/java/com/b/r/loteriab/r/Controllers/WebMvcConfig.java
@@ -25,7 +25,7 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("*");
     }
 
 }
